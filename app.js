5e3cb4203c4e4db09c6e04f11250be582f3cfbe4
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2023-10-07 14:48:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+//Importacion de librerias y cosas necesarias para correr el programa
 const express = require("express");
 const mongoose = require("mongoose");
 
-const userRoutes = require("./routes/routeuser");
-const orgRoutes = require("./routes/routeorg");
+//Guardamos las rutas en donde se encuentra el router de user y osc
+//Ojo que solo hice tablas para user y osc debido a que con un valor booleano podemos identificar con  true a un admin y false a un usuario
+//asi nos ahorramos una tabla
+const userRoutes = require("./routes/routeorg");
+const oscRoutes = require("./routes/routeuser");
 require("dotenv").config();
 
+//Declaracion de variables para la coneccion a base de datos en mongo
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //El puerto al que se conectara la aplicacion es al puerto 3000
 const MONGO_DB =
-  process.env.MONGO_DB || "mongodb+srv://santimtzv01:NK9d87DUH1NudDlR@cluster0.shiayac.mongodb.net/";
+  process.env.MONGO_DB || "mongodb://127.0.0.1:27017/frisa_app_db"; //link de coneccion a base de datos este varia en mi caso la base de datos la llame frisa_app_db lo demas se queda igual
 dbConnect();
 
+//Cuando en el link lea /users o /ocs accedera a las rutas ya declaradas previamente
 app.use(express.json());
 app.use("/users", userRoutes);
-app.use("/orgs/", orgRoutes);
+app.use("/osc", oscRoutes);
 
+//Mostramos en que puerto se esta corriendo el servidor
 app.listen(PORT, () => {
   console.log(`Servidor ejecutandose en el puerto: ${PORT}`);
 });
 
+//Mostramos que se haya conectado a la base de datos
 function dbConnect() {
   if (process.env.MONGO_DB) {
     console.log("Connecting to MongoDB in the cloud!");
