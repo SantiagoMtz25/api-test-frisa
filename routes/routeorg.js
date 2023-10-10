b6ba76e337860d54398d7cb3a78bb9869e60fcb0
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2023-10-10 14:31:01 -0600

    m

--- routes/routeorg.js
+++ routes/routeorg.js
@@ -4,10 +4,19 @@ const router = express.Router()
 const oscController = require('../controllers/oscController');
 
 //Get All OSC
-router.get('/', oscController.getAllOsc);
+router.get('/getAll/:name?', oscController.getAllOsc);
+
+//Org grade
+router.patch('/grade_org', oscController.orgGrade);
 
 //register OSC
 router.post('/register', oscController.oscRegister);
+
+//Update osc
+router.patch('/update_org', oscController.orgUpdateAcount);
+
+//login Osc
+router.put('/login', oscController.oscLogin);
 
 
 module.exports = router
