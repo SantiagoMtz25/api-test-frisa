d9a59784b4909f645c16b99d5fa954bc8e8016fd
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2023-10-17 20:39:02 -0600

    Backend

--- routes/routeradmin.js
+++ routes/routeradmin.js
@@ -10,19 +10,19 @@ router.get('/getAllOrgs', adminController.getAllOsc);
 router.get('/getAllUsers', adminController.getAllUsers);
 
 // Accept an OSC request
-router.put('/acceptOsc/:id', adminController.acceptOsc);
+//router.put('/acceptOsc/:id', adminController.acceptOsc);
 
 // Reject an OSC request
-router.delete('/rejectOsc/:id', adminController.rejectOsc);
+//router.delete('/rejectOsc/:id', adminController.rejectOsc);
 
 // Edit an OSC request
-router.put('/editOsc/:id', adminController.editOsc);
+//router.put('/editOsc/:id', adminController.editOsc);
 
 // Upload Excel 
-router.post('/uploadExcelOsc', adminController.uploadExcelOsc);
+//router.post('/uploadExcelOsc', adminController.uploadExcelOsc);
 
 // Get a specific OSC request by its ID
-router.get('/getOsc/:id', adminController.getOscById);
+//router.get('/getOsc/:id', adminController.getOscById);
 
 module.exports = router;
 
